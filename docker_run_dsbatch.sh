30110fccb5124ebcc4d36c087cce90d3444f978b
Greg Christian <greg.christian@smu.ca>
2025-09-15 13:28:45 -0300

    Added docker run script file

---
+++ docker_run_dsbatch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+docker run --rm -u $(id -u):$(id -g) \
+       -v /data/experiments/g3dragon:/data/experiments/g3dragon \
+       -w "$PWD" \
+       ghcr.io/gchristian321/docker_g3dragon:v2 \
+       bash -c "source ${1} && ./bin/dsbatch"
